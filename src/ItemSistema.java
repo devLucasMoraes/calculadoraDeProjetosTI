280e9adbd198537cc91cabbbb315e9474f20af15
ltrismegistus <trismegistolucas1@gmail.com>
2022-12-13 18:02:55 -0300

    criado classes ItemSistema.java, Profissional.java e enum Tipo.java

--- src/ItemSistema.java
+++ src/ItemSistema.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ItemSistema {
+public class ItemSistema {
+    private Tipo tipo;
+    private String nome;
+    private int complexidade;
+
+    public int CalculaEsforcoHoras(Tipo tipo) {
+
+        if(tipo.equals(tipo.TELAS_DE_CONSULTAS_COM_INFORMACAO_DE_DADOS)){
+            switch (complexidade) {
+                case 1:
+                    return 8;
+                case 2:
+                    return 12;
+                case 3:
+                    return 16;
+                default:
+                    return -1;
+            }
+        } else {
+            return 0;
+        }
+
+
+    }
 }
--- src/Profissional.java
+++ src/Profissional.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Profissional {
+public class Profissional {
+    private String Nome;
+    private double SalarioMensalDesejado = 1900;
+    private double FHD = 7;
+    private double diasUteisMensaisPadrao = 20;
+
+    public double CalcalaHorasMensais() {
+        return FHD * diasUteisMensaisPadrao;
+    }
+
+    public double CalculaValorHora() {
+        return (SalarioMensalDesejado * 2) / CalcalaHorasMensais();
+    }
 }
