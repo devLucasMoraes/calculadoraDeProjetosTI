38f0ea78696a4814c8e5d45e577695a609bca3bf
ltrismegistus <trismegistolucas1@gmail.com>
2022-12-15 09:12:33 -0300

    algumas melhorias no codigo da class ItemSistema.java

--- src/ItemSistema.java
+++ src/ItemSistema.java
@@ -11,50 +11,34 @@ public class ItemSistema {
 
     public int CalculaEsforcoHoras() {
 
-        if(this.tipo.equals(tipo.TELAS_DE_CONSULTAS_COM_INFORMACAO_DE_DADOS)){
-            switch (complexidade) {
-                case 1:
-                    return 4;
-                case 2:
-                    return 6;
-                case 3:
-                    return 8;
-                default:
-                    return -1;
-            }
-        } else if (this.tipo.equals(tipo.TELAS_COM_ENTRADAS_E_SAIDAS_DE_DADOS)) {
-            switch (complexidade){
-                case 1:
-                    return 8;
-                case 2:
-                    return 12;
-                case 3:
-                    return  16;
-                default:
-                    return -1;
-            }
-        } else if (this.tipo.equals(tipo.GRAVACOO_DE_DADOS)) {
-            switch (complexidade) {
-                case 1:
-                    return 16;
-                case 2:
-                    return 24;
-                case 3:
-                    return  36;
-                default:
-                    return -1;
-            }
-        } else if (this.tipo.equals((tipo.ALGORITMOS_COM_PROCESSAMENTOS))) {
-            switch (complexidade) {
-                case 1:
-                    return 16;
-                case 2:
-                    return 24;
-                case 3:
-                    return  36;
-                default:
-                    return -1;
-            }
+        if(this.tipo.equals(Tipo.TELAS_DE_CONSULTAS_COM_INFORMACAO_DE_DADOS)){
+            return switch (complexidade) {
+                case 1 -> 4;
+                case 2 -> 6;
+                case 3 -> 8;
+                default -> -1;
+            };
+        } else if (this.tipo.equals(Tipo.TELAS_COM_ENTRADAS_E_SAIDAS_DE_DADOS)) {
+            return switch (complexidade) {
+                case 1 -> 8;
+                case 2 -> 12;
+                case 3 -> 16;
+                default -> -1;
+            };
+        } else if (this.tipo.equals(Tipo.GRAVACOO_DE_DADOS)) {
+            return switch (complexidade) {
+                case 1 -> 16;
+                case 2 -> 24;
+                case 3 -> 36;
+                default -> -1;
+            };
+        } else if (this.tipo.equals((Tipo.ALGORITMOS_COM_PROCESSAMENTOS))) {
+            return switch (complexidade) {
+                case 1 -> 16;
+                case 2 -> 24;
+                case 3 -> 36;
+                default -> -1;
+            };
         } else {
             return -1;
         }
