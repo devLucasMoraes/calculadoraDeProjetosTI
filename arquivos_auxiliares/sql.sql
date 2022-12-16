a0c091c86c51e38f1d333667f99842e9a9faac2e
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-16 02:24:50 -0300

    libs auxiliares

--- arquivos_auxiliares/sql.sql
+++ arquivos_auxiliares/sql.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA `orcamentos_ti` ;
 CREATE TABLE `orcamentos_ti`.`orcamentos` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `profissional_nome` VARCHAR(45) NULL,
   `total_horas` DECIMAL(10,2) NULL,
   `custo_final` DECIMAL(10,2) NULL,
